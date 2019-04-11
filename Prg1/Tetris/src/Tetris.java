3e03c20b8bc4fb2c3101e4f5f94f8f685a952b7d
rasmus.karlsson11 <rasmus.karlsson@elev.ga.ntig.se>
2019-04-11 10:17:57 +0200

    Jag började med att göra olika blocks

---
+++ Prg1/Tetris/src/Tetris.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Tetris {
+
+    //L Block
+    {
+        { new Point(0, 1), new Point(1, 1), new Point(3,1),
+
+    }
+}
