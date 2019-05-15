cb335d671d076af4df8f6df4259177cfbee47368
rasmus.karlsson11 <rasmus.karlsson@elev.ga.ntig.se>
2019-05-15 14:55:40 +0200

    Notepad

--- Prg1/Notepad/src/Notepad.java
+++ Prg1/Notepad/src/Notepad.java
@@ -1,3 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Notepad {
+    public static void main (String[]args) {
+        String filename = "Notepad.txt";
+        FileReader fr =null;
+        try {
+            fr =new FileReader(filename);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        BufferedReader infile = new BufferedReader(fr);
+
+        String line;
+        try {
+            String inFile;
+            while ((line = inFile = infile.readLine()) !=null) {
+                System.out.println(line);
+            }
+            infile.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
