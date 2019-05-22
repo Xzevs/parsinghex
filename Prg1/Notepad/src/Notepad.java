d9cecdc3e35558683d7ced5c2bacc120e0fa027d
rasmus.karlsson11 <rasmus.karlsson@elev.ga.ntig.se>
2019-05-22 10:14:59 +0200

    fixade filer

--- Prg1/Notepad/src/Notepad.java
+++ Prg1/Notepad/src/Notepad.java
@@ -1,30 +1,61 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
 
-public class Notepad {
-    public static void main (String[]args) {
-        String filename = "Notepad.txt";
-        FileReader fr =null;
-        try {
-            fr =new FileReader(filename);
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
-        }
-        BufferedReader infile = new BufferedReader(fr);
+    public class Notepad {
+        private JTextArea textArea1;
+        private JButton saveButton;
+        private JButton openNewButton;
+        private JButton saveAsButton;
+        private JButton button1;
+        private JPanel NoTE17pad;
 
-        String line;
-        try {
-            String inFile;
-            while ((line = inFile = infile.readLine()) !=null) {
-                System.out.println(line);
+
+        public static void main(String[] args)  {
+            JFrame frame = new JFrame("Notepad");
+            frame.setContentPane(new Notepad().NoTE17pad);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+            // Öppna fil för läsning
+            JFileChooser fc = new JFileChooser();
+            int result = fc.showOpenDialog(null);
+            String filename;
+            if (result == JFileChooser.APPROVE_OPTION) {
+                filename = fc.getSelectedFile().getAbsolutePath();
+            } else {
+                filename = "Notepad.txt";
             }
-            infile.close();
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            FileReader fr = null;
+            try {
+                fr = new FileReader(filename);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            BufferedReader inFile = new BufferedReader(fr);
+
+            //Öppna fil för skrivning
+            String filename2 = filename+"Copy";
+            FileWriter fw = null;
+            try {
+                fw = new FileWriter(filename2);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter outFile = new PrintWriter(bw);
+
+            // Läs in filen
+            String line;
+            try {
+                while ((line = inFile.readLine() ) != null) {
+                    outFile.println(line);
+                }
+                inFile.close();
+                outFile.flush();
+                outFile.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
-
-}
